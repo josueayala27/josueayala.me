7213da853797d8dc6afb14a92835364fdd37248a
Josué Ayala <ayalavalencia11@gmail.com>
2022-07-16 19:39:25 -0600

    🧂 Removed plugin supabase

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-export default ({ $config }: any, inject: any) => {
-  const supabase = createClient(
-    $config.NUXT_PUBLIC_SUPABASE_URL,
-    $config.NUXT_PUBLIC_SUPABASE_ANON_KEY
-  );
-  inject('supabase', supabase);
-};
+// export default ({ $config }: any, inject: any) => {
+//   const supabase = createClient(
+//     $config.NUXT_PUBLIC_SUPABASE_URL,
+//     $config.NUXT_PUBLIC_SUPABASE_ANON_KEY
+//   );
+//   inject('supabase', supabase);
+// };
