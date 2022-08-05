b611ac10dacf397a75e94e5dcc5859bc504a3897
Restyled.io <commits@restyled.io>
2022-08-05 03:22:16 +0000

    Restyled by clang-format

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import { getHighlighter } from 'shiki';
+import {getHighlighter} from 'shiki';
 
 export default {
   head: {
@@ -6,51 +6,55 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { hid: 'twitter:site', name: 'twitter:site', content: '@joscode' },
-      {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary_large_image',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    meta:
+        [
+          {charset : 'utf-8'},
+          {name : 'viewport', content : 'width=device-width, initial-scale=1'},
+          {hid : 'description', name : 'description', content : ''},
+          {name : 'format-detection', content : 'telephone=no'},
+          {hid : 'twitter:site', name : 'twitter:site', content : '@joscode'},
+          {
+            hid : 'twitter:card',
+            name : 'twitter:card',
+            content : 'summary_large_image',
+          },
+        ],
+    link: [ {rel : 'icon', type : 'image/x-icon', href : '/favicon.ico'} ],
   },
 
-  css: ['@/assets/css/main.css'],
+  css: [ '@/assets/css/main.css' ],
 
-  plugins: [
-    '@/plugins/firebase.client.ts',
-    '@/plugins/filters.ts',
-    '@/plugins/dayjs.ts',
-    '@/plugins/v-click-outside.client.ts',
-    '@/plugins/v-sanitize.client.ts',
-  ],
+  plugins:
+      [
+        '@/plugins/firebase.client.ts',
+        '@/plugins/filters.ts',
+        '@/plugins/dayjs.ts',
+        '@/plugins/v-click-outside.client.ts',
+        '@/plugins/v-sanitize.client.ts',
+      ],
 
   components: {
     global: true,
-    dirs: [
-      '@/components/ui',
-      '@/components/ui/typography',
-      '@/components/ui/dropdown',
-      '@/components/blog',
-      '@/components/blog/using-lottie-files-with-nuxtjs',
-      '@/components/layout',
-    ],
+    dirs:
+        [
+          '@/components/ui',
+          '@/components/ui/typography',
+          '@/components/ui/dropdown',
+          '@/components/blog',
+          '@/components/blog/using-lottie-files-with-nuxtjs',
+          '@/components/layout',
+        ],
   },
 
-  buildModules: [
-    '@nuxt/typescript-build',
-    '@nuxt/postcss8',
-    '@nuxtjs/svg',
-    '@nuxtjs/google-fonts',
-  ],
+  buildModules:
+      [
+        '@nuxt/typescript-build',
+        '@nuxt/postcss8',
+        '@nuxtjs/svg',
+        '@nuxtjs/google-fonts',
+      ],
 
-  modules: ['@nuxtjs/axios', '@nuxt/content'],
+  modules: [ '@nuxtjs/axios', '@nuxt/content' ],
 
   build: {
     postcss: {
@@ -67,8 +71,8 @@ export default {
     download: true,
     overwriting: false,
     families: {
-      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      'JetBrains Mono': [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      Inter: [ 100, 200, 300, 400, 500, 600, 700, 800, 900 ],
+      'JetBrains Mono': [ 100, 200, 300, 400, 500, 600, 700, 800, 900 ],
     },
   },
 
@@ -76,7 +80,7 @@ export default {
     markdown: {
       async highlighter() {
         const highlighter = await getHighlighter({
-          theme: 'dracula',
+          theme : 'dracula',
         });
         return (code, lang) => highlighter.codeToHtml(code, lang);
       },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  darkMode: ['class'],
-  content: [
+  darkMode : [ 'class' ],
+  content : [
     './components/**/*.{js,vue,ts}',
     './content/**/*.md',
     './layouts/**/*.vue',
@@ -8,28 +8,28 @@ module.exports = {
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
-  theme: {
-    fontFamily: {
-      sans: ['Inter', 'sans-serif'],
-      mono: ['JetBrains Mono', 'sans-serif'],
+  theme : {
+    fontFamily : {
+      sans : [ 'Inter', 'sans-serif' ],
+      mono : [ 'JetBrains Mono', 'sans-serif' ],
     },
-    extend: {
-      colors: {
-        'pastel-green': {
-          DEFAULT: '#66D88C',
-          50: '#F8FDFA',
-          100: '#E8F9EE',
-          200: '#C8F1D5',
-          300: '#A7E9BD',
-          400: '#87E0A4',
-          500: '#66D88C',
-          600: '#39CD6A',
-          700: '#2AA453',
-          800: '#1E773C',
-          900: '#134B26',
+    extend : {
+      colors : {
+        'pastel-green' : {
+          DEFAULT : '#66D88C',
+          50 : '#F8FDFA',
+          100 : '#E8F9EE',
+          200 : '#C8F1D5',
+          300 : '#A7E9BD',
+          400 : '#87E0A4',
+          500 : '#66D88C',
+          600 : '#39CD6A',
+          700 : '#2AA453',
+          800 : '#1E773C',
+          900 : '#134B26',
         },
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins : [ require('@tailwindcss/typography') ],
 };
