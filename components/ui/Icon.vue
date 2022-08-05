00c6afdacaadabc2254f27deec85a8f9481cc6ad
Josué Ayala <ayalavalencia11@gmail.com>
2022-08-04 22:08:56 -0600

    👌🏼 `_slug.vue` refactor

--- components/ui/Icon.vue
+++ components/ui/Icon.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-html="icon"
-    @click="$emit('click')"
+    @click="handleClick()"
     :style="{ width: size, height: size }"
     class="fill-current relative z-0 align-top inline-flex"></div>
 </template>
@@ -13,10 +13,20 @@ export default {
     category: { type: String, default: 'outline' },
     name: { type: String, default: '' },
     size: { type: String, default: '1.2rem' },
+    href: { type: String, default: '' },
   },
   computed: {
     icon() {
       return require(`@/assets/icons/${this.category}/${this.name}.svg?raw`);
+    },
+  },
+  methods: {
+    handleClick() {
+      if (this.href) {
+        window.open(this.href, '_blank');
+        return;
+      }
+      this.$emit('click');
     },
   },
 };
