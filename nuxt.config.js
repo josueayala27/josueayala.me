c456e7110bc74b48be4c5e115ed9c6212ea9bff6
Josué Ayala <ayalavalencia11@gmail.com>
2022-08-08 14:26:18 -0600

    😍 Firebase implementation

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
     '@/plugins/dayjs.ts',
     '@/plugins/v-click-outside.client.ts',
     '@/plugins/v-sanitize.client.ts',
+    '@/plugins/firebase.client.ts',
   ],
 
   components: {
@@ -61,6 +62,17 @@ export default {
         autoprefixer: {},
       },
     },
+  },
+
+  publicRuntimeConfig: {
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   },
 
   content,
