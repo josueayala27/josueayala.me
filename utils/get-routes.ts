05bc9a797343bc4e38edc2150064885585fb3bae
Josué Ayala <ayalavalencia11@gmail.com>
2022-08-23 08:07:49 -0600

    Revert "Develop"

--- utils/get-routes.ts
+++
@@ -1,6 +0,0 @@
-export default async () => {
-  const { $content } = require('@nuxt/content');
-  const files = await $content({ deep: true }).only(['path']).fetch();
-
-  return files.map((file: any) => (file.path === '/index' ? '/' : file.path));
-};
