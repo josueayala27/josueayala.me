03480f9bbf0a94eea3a36bbf4bd370331bb576fe
Josué Ayala <ayalavalencia11@gmail.com>
2022-09-11 13:17:17 -0600

    🌙 `ts.config.json` was updated

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@ export default {
   async asyncData({ $content, app }) {
     const posts = await $content(app.i18n.locale)
       .limit(5)
-      .sortBy('date', 'desc')
+      .sortBy('date', 'asc')
       .where({ isPublished: true })
       .only(['description', 'title', 'slug'])
       .fetch();
